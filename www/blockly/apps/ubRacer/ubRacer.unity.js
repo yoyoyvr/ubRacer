61483a44e57b321630513434e54baf3a3bd2c683
yggy <yggy@blackbirdinteractive.com>
2014-10-22 22:18:11 -0700

    Put ubRacer.unity stuff into a namespace.

--- www/blockly/apps/ubRacer/ubRacer.unity.js
+++ www/blockly/apps/ubRacer/ubRacer.unity.js
@@ -27,47 +27,58 @@
   * html generated when making a web build.
   */
 
-var config = {
-    width: 960, 
-    height: 600,
-    params: { enableDebugging:"0" }
+// Put everything in a namespace.
+var ubRacer = {
+
+    init : function() {
+        var config = {
+            width: 960, 
+            height: 600,
+            params: { enableDebugging:"0" }
+        };
+        
+        ubRacer.uPlayer = new UnityObject2(ubRacer.config);
+        
+        jQuery(ubRacer.initWebPlayer);
+    },
     
+    initWebPlayer : function() {
+        var $missingScreen = jQuery("#unityPlayer").find(".missing");
+        var $brokenScreen = jQuery("#unityPlayer").find(".broken");
+        $missingScreen.hide();
+        $brokenScreen.hide();
+        
+        ubRacer.uPlayer.observeProgress(function (progress) {
+            switch(progress.pluginStatus) {
+                case "broken":
+                    $brokenScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        ubRacer.uPlayer.installPlugin();
+                        return false;
+                    });
+                    $brokenScreen.show();
+                break;
+                case "missing":
+                    $missingScreen.find("a").click(function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        ubRacer.uPlayer.installPlugin();
+                        return false;
+                    });
+                    $missingScreen.show();
+                break;
+                case "installed":
+                    $missingScreen.remove();
+                break;
+                case "first":
+                break;
+            }
+        });
+        
+        ubRacer.uPlayer.initPlugin(jQuery("#unityPlayer")[0], "racer.unity3d");
+    }
 };
-var uPlayer = new UnityObject2(config);
 
-jQuery(function() {
-
-    var $missingScreen = jQuery("#unityPlayer").find(".missing");
-    var $brokenScreen = jQuery("#unityPlayer").find(".broken");
-    $missingScreen.hide();
-    $brokenScreen.hide();
-    
-    u.observeProgress(function (progress) {
-        switch(progress.pluginStatus) {
-            case "broken":
-                $brokenScreen.find("a").click(function (e) {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    u.installPlugin();
-                    return false;
-                });
-                $brokenScreen.show();
-            break;
-            case "missing":
-                $missingScreen.find("a").click(function (e) {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    u.installPlugin();
-                    return false;
-                });
-                $missingScreen.show();
-            break;
-            case "installed":
-                $missingScreen.remove();
-            break;
-            case "first":
-            break;
-        }
-    });
-    uPlayer.initPlugin(jQuery("#unityPlayer")[0], "racer.unity3d");
-});
+// Bootstrap the player.
+ubRacer.init();
